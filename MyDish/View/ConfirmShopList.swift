773aa597e25919c1272c737d96879e8d1ff205ea
Pawel <pawel3007@vp.pl>
2020-01-27 17:22:07 +0100

    push repo to git

--- MyDish/View/ConfirmShopList.swift
+++ MyDish/View/ConfirmShopList.swift
@@ -15,7 +15,6 @@ class ConfirmShopList: UIView{
     
     @IBOutlet weak var confirmTable: UITableView!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = .systemGray6
