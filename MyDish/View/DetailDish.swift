18430fb30ef54c824f6ed597823bf1f2dfdb5686
Pawel <pawel3007@vp.pl>
2020-02-14 17:36:30 +0100

    refactor autolayout detaildishview

--- MyDish/View/DetailDish.swift
+++ MyDish/View/DetailDish.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class DetailDishView:UIView{
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var directionView: UIView!
     @IBOutlet private weak var directionTable: UITableView!
     @IBOutlet private weak var ingredientsTable: UITableView!
     @IBOutlet private weak var shopListBtn: UIButton!
@@ -20,8 +23,11 @@ class DetailDishView:UIView{
     @IBOutlet private weak var cookingTimeLbl: UILabel!
     @IBOutlet private weak var timeView: UIView!
     @IBOutlet private weak var img: UIImageView!
+    @IBOutlet weak var topView: UIView!
     
     private let detailVC = DetailDishViewController()
+    @IBOutlet weak var ingredientsTableHeight: NSLayoutConstraint!
+    @IBOutlet weak var directionTableHeight: NSLayoutConstraint!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,29 +40,76 @@ class DetailDishView:UIView{
     override func awakeFromNib() {
         super.awakeFromNib()
         setTimeView()
-        setIngredientsLbl()
-        setDirectionsLbl()
-        deleteExtraEmptyCell()
-        setCookingTimeLbl()
+               setIngredientsLbl()
+               setDirectionsLbl()
+               deleteExtraEmptyCell()
+               setCookingTimeLbl()
+               scrollAutoLayout()
+               containerAutoLayout()
+               topViewAutoLayout()
+               directionViewAutoLayout()
     }
-//    getters
-    func getIngredientsTable() -> UITableView{
-          return ingredientsTable
-      }
-      
-      func getShopListBtn() -> UIButton{
-          return shopListBtn
-      }
-      
-      func getCookLaterBtn() -> UIButton{
-          return cookLaterBtn
-      }
-      
-      func getDirectionsTable() -> UITableView{
-          return directionTable
-      }
     
-//    set view func
+    func setHeightIngredientsTable(_ height: CGFloat){
+        ingredientsTableHeight.constant = height
+    }
+    
+    func setHeightDirectionTable(_ height: CGFloat){
+        directionTableHeight.constant = height + 50
+    }
+    
+    private func scrollAutoLayout() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+    }
+    
+    private func containerAutoLayout() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+    }
+    
+    private func topViewAutoLayout() {
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        topView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        topView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        topView.widthAnchor.constraint(equalToConstant: 375).isActive = true
+    }
+    
+    private func directionViewAutoLayout() {
+        directionView.translatesAutoresizingMaskIntoConstraints = false
+        directionView.topAnchor.constraint(equalTo: timeView.bottomAnchor,constant: 28).isActive = true
+        directionView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        directionView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        directionView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+    }
+    
+    //    getters
+    func getIngredientsTable() -> UITableView{
+        return ingredientsTable
+    }
+    
+    func getShopListBtn() -> UIButton{
+        return shopListBtn
+    }
+    
+    func getCookLaterBtn() -> UIButton{
+        return cookLaterBtn
+    }
+    
+    func getDirectionsTable() -> UITableView{
+        return directionTable
+    }
+    
+    //    set view func
     
     private func deleteExtraEmptyCell(){
         ingredientsTable.tableFooterView = UIView()
@@ -64,6 +117,12 @@ class DetailDishView:UIView{
     }
     
     private func setTimeView(){
+        timeView.translatesAutoresizingMaskIntoConstraints = false
+        timeView.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: 10).isActive = true
+        timeView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+        timeView.widthAnchor.constraint(equalToConstant: 203).isActive = true
+        timeView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
         timeView.layer.borderWidth = 1
         timeView.layer.borderColor = UIColor.systemGray.cgColor
         timeView.layer.cornerRadius = 10
@@ -97,6 +156,12 @@ class DetailDishView:UIView{
     
     func setImg(_ imgName: String){
         img.image = UIImage(named: imgName)
+        
+        self.img.translatesAutoresizingMaskIntoConstraints = false
+        self.img.topAnchor.constraint(equalTo: topView.topAnchor, constant: -68).isActive = true
+        self.img.leadingAnchor.constraint(equalTo: topView.leadingAnchor).isActive = true
+        self.img.trailingAnchor.constraint(equalTo: topView.trailingAnchor).isActive = true
+        self.img.heightAnchor.constraint(equalToConstant: 371).isActive = true
     }
     
     func setNavigation(_ navigationController: UINavigationController){
